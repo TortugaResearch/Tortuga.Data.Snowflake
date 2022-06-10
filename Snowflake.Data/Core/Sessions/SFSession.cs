977ef8552b3d35bcde0ea75058f79f4d576c3477
Jonathan Allen <grauenwolf@gmail.com>
2022-06-10 01:25:28 -0700

    Remove unnecessary visibility modifies.

--- Snowflake.Data/Core/Sessions/SFSession.cs
+++ Snowflake.Data/Core/Sessions/SFSession.cs
@@ -2,6 +2,8 @@
  * Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
  */
 
+#nullable enable
+
 using Microsoft.AspNetCore.WebUtilities;
 using System.Net;
 using System.Security;
--- Snowflake.Data/Core/Sessions/SFSessionParameter.cs
+++ Snowflake.Data/Core/Sessions/SFSessionParameter.cs
@@ -2,6 +2,8 @@
  * Copyright (c) 2012-2019 Snowflake Computing Inc. All rights reserved.
  */
 
+#nullable enable
+
 namespace Tortuga.Data.Snowflake.Core.Sessions;
 
 internal enum SFSessionParameter
